63f8cc714495acebd17242e1f8f66208a7d72fbd
Ilia Machkarski <odeliks12@gmail.com>
2021-04-01 13:02:17 +0300

    Added default deck, draw card from deck, swap 2 cards in the deck, suit counter

--- src/model/deck.cpp
+++ src/model/deck.cpp
@@ -1,1 +1,39 @@
 #include "../../include/deck.h"
+#include <cstring>
+
+//default deck with 52 cards (each once)
+Deck::Deck() {
+    deckSize = 52;
+    //12 types of cards Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King
+    for(int value = 0; value < 13; value++) {
+        //4 suits Clubs, Diamonds, Hearts, Spades
+        for(int suit = 0; suit < 5; suit++) {
+            Card tempCard(value, suit, "def");
+            sequence.push_back(tempCard);
+        }
+    }
+    strcpy(series, "Default");
+}
+// return first card of the deck and pushes it at the end of the deck 
+Card Deck::Draw() {
+    Card first(sequence[0]);
+    sequence.pop_front();
+    sequence.push_back(first);
+
+    return first;
+}
+// swaps 2 cards with indexes a, b(changed header with corect arguments)
+void Deck::Swap(int a, int b) {
+    Card temp = sequence[a];
+    sequence[a] = sequence[b];
+    sequence[b] = temp;
+}
+// returns how many times a suit is found
+int Deck::suit_count(Suit& suit) const {
+    int sum = 0;
+    for(int i = 0; i < 52; i++) {
+        if(this->sequence[i].getSuit() == suit)
+        sum += 1;
+    }
+    return sum;
+}
